0ceaaf35c612968d65a113c4d10bb17f85e4a2dd
Carola Heinzel <164475282+CarolaHeinzel@users.noreply.github.com>
2025-03-04 11:00:26 +0100

    Update own_idea.R

--- own_idea.R
+++ own_idea.R
@@ -1,5 +1,5 @@
 # Calculates the Estimator with the new idea, i.e. \hat r_{new}
-
+source("Simulation_self.R")
 is_valid_permutation <- function(h, k, n) {
   if (k > 1) {
     if (h[k] > max(h[1:(k-1)])) {
@@ -18,53 +18,6 @@ is_valid_permutation <- function(h, k, n) {
     }
   }
   return(TRUE)
-}
-
-
-calculate_leaf_info <- function(tree_data, n) {
-  # Calculate the leaves
-  leaves <- setdiff(tree_data$Node, tree_data$Parent)
-  
-  get_leaves <- function(node, tree) {
-    children <- tree$Node[tree$Parent == node]
-    if (length(children) == 0) {
-      return(node)
-    } else {
-      return(unlist(lapply(children, get_leaves, tree = tree)))
-    }
-  }
-  
-  results <- data.frame(Edge = integer(), Leaves = integer(), Total_Length = numeric())
-  # Calculate the lenght of branches that support k = 2,.., n-1 leaves
-  for (i in 1:nrow(tree_data)) {
-    node <- tree_data$Node[i]
-    edge_length <- tree_data$Edge_length[i]
-    
-    descendant_leaves <- get_leaves(node, tree_data)
-    
-    num_leaves <- length(descendant_leaves)
-    total_length <- edge_length
-    
-    results <- rbind(
-      results,
-      data.frame(Edge = node, Leaves = num_leaves, Total_Length = total_length)
-    )
-  }
-  
-  summed_results <- aggregate(Total_Length ~ Leaves, data = results, FUN = sum)
-  
-  check_leaves <- data.frame(Leaves = 1:(n - 1), Exists = FALSE, Total_Length_Positive = FALSE)
-  
-  for (k in check_leaves$Leaves) {
-    # Check wheter k occurs
-    exists <- any(results$Leaves == k)
-    positive_length <- any(results$Leaves == k & results$Total_Length > 0)
-    
-    
-    check_leaves$Exists[check_leaves$Leaves == k] <- exists
-    check_leaves$Total_Length_Positive[check_leaves$Leaves == k] <- positive_length
-  }
-  return(list(results = results, summed_results = summed_results, check_leaves = check_leaves))
 }
 
 ew_durrett_adapted <- function(n, tree_data){
