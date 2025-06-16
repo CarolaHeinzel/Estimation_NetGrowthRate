7812be4ab349421f22e499407b5bc7a120cc628e
Carola Heinzel <164475282+CarolaHeinzel@users.noreply.github.com>
2025-06-16 21:49:45 +0200

    Create New_Estimator.R

---
+++ New_Estimator.R
@@ -0,0 +1,24 @@
+# Calculation of the new estimator with constant n
+
+# Calculates ∑_i ∑_j (H_i - H_j)^+
+# h is a list that contains the coalescence times
+compute_list_properties <- function(h) {
+  double_sum <- sum(outer(h, h, function(x, y) pmax(0, x - y)))
+  return(double_sum)
+}
+
+# Calculates \hat r_{new} with the constant c (which has to be determined)
+# n is the samples size
+calc_estimator <- function(h, c){
+  n = length(h) + 1
+  values <- compute_list_properties(h)
+  est <-  1/(n-1)*values*c
+  return(est)
+}
+
+# Exaple application
+h = c(1,4,5,2,3)
+c = 1
+r = calc_estimator(h, c)
+
+
